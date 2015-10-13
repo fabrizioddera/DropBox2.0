f1a8ac47bc07e34fe199969b85601bf5d0ddc98c
alema92 <alessia.mantovani@studenti.polito.it>
2015-10-13 10:28:27 +0200

    aggiunte cose

---
+++ WindowsFormsApplication3_SERVER-CLIENT2 web/WindowsFormsApplication3_SERVER-CLIENT/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace WindowsFormsApplication3_SERVER_CLIENT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string n;
+        byte[] b1;
+        OpenFileDialog op;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                string t = textBox1.Text;
+                t = op.FileName;
+                FileInfo fi = new FileInfo(textBox1.Text = op.FileName);
+                n = fi.Name + "." + fi.Length;
+                TcpClient client = new TcpClient("Pc-TOSH", 5050);
+                StreamWriter sw = new StreamWriter(client.GetStream());
+                sw.WriteLine(n);
+                sw.Flush();
+                label1.Text = "Client connected....";
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient("Pc-TOSH", 5050);
+            Stream s = client.GetStream();
+            b1 = File.ReadAllBytes(op.FileName);
+            s.Write(b1, 0, b1.Length);
+            client.Close();
+            label1.Text = "File Transferred....";
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+        }
+    }
+} 
+
---
+++ WindowsFormsApplication3_SERVER-CLIENT2 web/WindowsFormsApplication3_SERVER-CLIENT/Form2.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace WindowsFormsApplication3_SERVER_CLIENT
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        SocketPermission permission;
+        Socket sListener;
+        IPEndPoint ipEndPoint;
+        Socket handler;
+
+        string rd;
+        byte[] b1;
+        string v;
+        int m;
+        TcpListener list;
+        Int32 port = 4510;
+        Int32 port1 = 5055;
+       //IPAddress localAddr = IPAddress.Parse("192.168.1.20");
+    private void Browse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                //TcpListener list = new TcpListener(localAddr,port1);
+                list = new TcpListener(port1);
+                list.Start();
+                TcpClient client = list.AcceptTcpClient();
+                Stream s = client.GetStream();
+                b1 = new byte[m];
+                s.Read(b1, 0, b1.Length);
+                File.WriteAllBytes(textBox1.Text + "\\" + rd.Substring(0, rd.LastIndexOf('.')), b1);
+                list.Stop();
+                client.Close();
+                label4.Text = "File Received......";
+            }
+         }        
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //TcpListener list = new TcpListener(localAddr, port);
+            TcpListener list = new TcpListener(port);
+            list.Start();
+            TcpClient client = list.AcceptTcpClient();
+            MessageBox.Show("Client trying to connect");
+            StreamReader sr = new StreamReader(client.GetStream());
+            rd = sr.ReadLine();
+            v = rd.Substring(rd.LastIndexOf('.') + 1);
+            m = int.Parse(v);
+            list.Stop();
+            client.Close();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of FolderBrowserDialog.
+            FolderBrowserDialog folderBrowserDlg = new FolderBrowserDialog();
+            // A new folder button will display in FolderBrowserDialog.
+            folderBrowserDlg.ShowNewFolderButton = true;
+            //Show FolderBrowserDialog
+            DialogResult dlgResult = folderBrowserDlg.ShowDialog();
+            if (dlgResult.Equals(DialogResult.OK))
+            {
+                //Show selected folder path in textbox1.
+                textBox1.Text = folderBrowserDlg.SelectedPath;
+                //Browsing start from root folder.
+                Environment.SpecialFolder rootFolder = folderBrowserDlg.RootFolder;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Creates one SocketPermission object for access restrictions
+                permission = new SocketPermission(
+                NetworkAccess.Accept,     // Allowed to accept connections 
+                TransportType.Tcp,        // Defines transport types 
+                "",                       // The IP addresses of local host 
+                SocketPermission.AllPorts // Specifies all ports 
+                );
+
+                // Listening Socket object 
+                sListener = null;
+
+                // Ensures the code to have permission to access a Socket 
+                permission.Demand();
+
+                // Resolves a host name to an IPHostEntry instance 
+                IPHostEntry ipHost = Dns.GetHostEntry("");
+
+                // Gets first IP address associated with a localhost 
+                IPAddress ipAddr = ipHost.AddressList[0];
+
+                // Creates a network endpoint 
+                ipEndPoint = new IPEndPoint(ipAddr, 4510);
+
+                // Create one Socket object to listen the incoming connection 
+                sListener = new Socket(
+                    ipAddr.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp
+                    );
+
+                // Associates a Socket with a local endpoint 
+                sListener.Bind(ipEndPoint);
+
+                label1.Text = "Server started.";
+
+              
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+
+            try
+            {
+                // Places a Socket in a listening state and specifies the maximum 
+                // Length of the pending connections queue 
+                sListener.Listen(10);
+
+                // Begins an asynchronous operation to accept an attempt 
+                AsyncCallback aCallback = new AsyncCallback(AcceptCallback);
+                sListener.BeginAccept(aCallback, sListener);
+
+                label3.Text = "Server is now listening on " + ipEndPoint.Address + " port: " + ipEndPoint.Port;
+
+               
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            Socket listener = null;
+
+            // A new Socket to handle remote host communication 
+            Socket handler = null;
+            try
+            {
+                // Receiving byte array 
+                byte[] buffer = new byte[1024];
+                // Get Listening Socket object 
+                listener = (Socket)ar.AsyncState;
+                // Create a new socket 
+                handler = listener.EndAccept(ar);
+
+                // Using the Nagle algorithm 
+                handler.NoDelay = false;
+
+                // Creates one object array for passing data 
+                object[] obj = new object[2];
+                obj[0] = buffer;
+                obj[1] = handler;
+
+                // Begins to asynchronously receive data 
+                handler.BeginReceive(
+                    buffer,        // An array of type Byt for received data 
+                    0,             // The zero-based position in the buffer  
+                    buffer.Length, // The number of bytes to receive 
+                    SocketFlags.None,// Specifies send and receive behaviors 
+                    new AsyncCallback(ReceiveCallback),//An AsyncCallback delegate 
+                    obj            // Specifies infomation for receive operation 
+                    );
+
+                // Begins an asynchronous operation to accept an attempt 
+                AsyncCallback aCallback = new AsyncCallback(AcceptCallback);
+                listener.BeginAccept(aCallback, listener);
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+        }
+
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Fetch a user-defined object that contains information 
+                object[] obj = new object[2];
+                obj = (object[])ar.AsyncState;
+
+                // Received byte array 
+                byte[] buffer = (byte[])obj[0];
+
+                // A Socket to handle remote host communication. 
+                handler = (Socket)obj[1];
+
+                // Received message 
+                string content = string.Empty;
+
+
+                // The number of bytes received. 
+                int bytesRead = handler.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    content += Encoding.Unicode.GetString(buffer, 0,
+                        bytesRead);
+
+                    // If message contains "<Client Quit>", finish receiving
+                    if (content.IndexOf("<Client Quit>") > -1)
+                    {
+                        // Convert byte array to string
+                        string str = content.Substring(0, content.LastIndexOf("<Client Quit>"));
+
+                       
+                    }
+                    else
+                    {
+                        // Continues to asynchronously receive data
+                        byte[] buffernew = new byte[1024];
+                        obj[0] = buffernew;
+                        obj[1] = handler;
+                        handler.BeginReceive(buffernew, 0, buffernew.Length,
+                            SocketFlags.None,
+                            new AsyncCallback(ReceiveCallback), obj);
+                    }
+
+                    
+                }
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+        }
+
+      
+
+        public void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // A Socket which has sent the data to remote host 
+                Socket handler = (Socket)ar.AsyncState;
+
+                // The number of bytes sent to the Socket 
+                int bytesSend = handler.EndSend(ar);
+                Console.WriteLine(
+                    "Sent {0} bytes to Client", bytesSend);
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+        }
+
+       
+        }
+
+
+
+
+
+
+}
