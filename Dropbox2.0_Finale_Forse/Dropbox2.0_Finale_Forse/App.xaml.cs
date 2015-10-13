e3165cfa8155b0fac56ef2db7f799c0d872d57d9
fabrizioddera <fabrizioddera@gmail.com>
2015-10-13 11:26:52 +0200

    Aggiunta
    Aggiunto progetto finale

---
+++ Dropbox2.0_Finale_Forse/Dropbox2.0_Finale_Forse/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dropbox2._0_Finale_Forse
+{
+    /// <summary>
+    /// Logica di interazione per App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
