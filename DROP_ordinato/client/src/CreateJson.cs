876a33c943d5760d79bd36e0175ebc3e55a46371
alema92 <alessia.mantovani@studenti.polito.it>
2015-11-19 16:55:53 +0100

    Watcher, Json, ExploreTree added

---
+++ DROP_ordinato/client/src/CreateJson.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.src
+{
+    class CreateJson
+    {
+
+         //string value = @"C:\Users\Pc\Desktop\Nuova cartella";
+         //var json = GetDirectory(new DirectoryInfo(value)).ToString();
+         //Console.WriteLine(json);
+
+
+        JToken GetDirectory(DirectoryInfo directory)
+        {
+            return JToken.FromObject(new
+            {
+                directory = directory.EnumerateDirectories().ToDictionary(x => x.Name, x => GetDirectory(x)),
+                file = directory.EnumerateFiles().Select(x => x.Name).ToList()
+            });
+        }
+
+    }
+}
